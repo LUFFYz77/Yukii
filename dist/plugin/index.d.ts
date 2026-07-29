701c30c0f332d834ad6833b0a487a9afb2108918
LUFFYz77 <lh2295484@gmail.com>
2026-07-29 21:40:22 +0700

    Add files via upload

--- dist/plugin/index.d.ts
+++ dist/plugin/index.d.ts
@@ -1,1 +1,10 @@
-
+import type { Client } from '../structures/index';
+export default function loadPlugins(client: Client): Promise<void>;
+export interface BotPlugin {
+    name: string;
+    version: string;
+    author: string;
+    description?: string;
+    initialize: (client: Client) => void;
+    shutdown?: (client: Client) => void;
+}
