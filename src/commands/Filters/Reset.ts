1deb288d8d63383b64f4e5b4ae836bb37c80db1c
LUFFYz77 <lh2295484@gmail.com>
2026-07-29 17:47:22 +0700

    Reset.ts
    Implements a Reset command to reset audio filters and equalizer settings.

---
+++ src/commands/Filters/Reset.ts
@@ -0,0 +1,48 @@
+import { Command, type Context, type Client } from '../../structures/index.js';
+
+export default class Reset extends Command {
+	constructor(client: Client) {
+		super(client, {
+			name: 'reset',
+			description: {
+				content: 'cmd.reset.description',
+				examples: ['reset'],
+				usage: 'reset',
+			},
+			category: 'filters',
+			aliases: ['rs'],
+			cooldown: 3,
+			args: false,
+			vote: false,
+			player: {
+				voice: true,
+				dj: true,
+				active: false,
+				djPerm: null,
+			},
+			permissions: {
+				dev: false,
+				client: ['SendMessages', 'ReadMessageHistory', 'ViewChannel', 'EmbedLinks'],
+				user: [],
+			},
+			slashCommand: true,
+			options: [],
+		});
+	}
+
+	public async run(client: Client, ctx: Context): Promise<any> {
+		const player = client.manager.getPlayer(ctx.guild!.id);
+		if (!player) return await ctx.sendMessage(ctx.locale('event.message.no_music_playing'));
+		player.filterManager.resetFilters();
+		player.filterManager.clearEQ();
+		await ctx.sendMessage({
+			embeds: [
+				{
+					description: ctx.locale('cmd.reset.messages.filters_reset'),
+					color: this.client.color.main,
+				},
+			],
+		});
+	}
+}
+
